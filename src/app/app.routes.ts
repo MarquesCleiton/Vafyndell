d4a067dc2b55589a24819a58c3de60aa281b5172
marquescleiton <cleiton.contato2210@gmail.com>
2025-09-06 11:06:55 -0300

    página de login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,16 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  {
-    path: 'home',
-    loadComponent: () =>
-      import('./pages/home/home').then((m) => m.Home),
-  },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
   {
     path: 'login',
     loadComponent: () =>
       import('./pages/login/login').then((m) => m.Login),
   },
+  {
+    path: 'home',
+    loadComponent: () =>
+      import('./pages/home/home').then((m) => m.Home),
+  },
+  { path: '**', redirectTo: 'login' }
 ];
